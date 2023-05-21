07b8d5adbd060daf037464f8d99c149b4d8b5a14
робот <vaginalnaymamba@mail.ru>
2023-05-22 00:30:31 +0300

    finish hm

---
+++ src/Bicycle.java
@@ -0,0 +1,17 @@
+public class Bicycle extends Transport {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+
+    public void doSorvice() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,11 @@
+public class ServiceStation {
+    public void check(Bicycle bicycle){
+        bicycle.doSorvice();
+    }
+    public void check(Car car){
+        car.doSorvice();
+    }
+    public void check(Truck truck){
+        truck.doSorvice();
+    }
+}
